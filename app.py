e5aac113c6e1abb4e21be3d4320c4e0bd8c4542d
Rick Cavalla <monolith32@gmail.com>
2019-09-15 01:54:54 -0400

    Final updates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+
+# import our Mars scraping library
 import scrape_mars
 
 app = Flask(__name__)
@@ -11,20 +13,25 @@ mongo = PyMongo(app)
 # Or set inline
 # mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
 
-
+# home route will just find the single Mongo collection
+# of our mars data and pass that to index page
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
-
+# scrape route will be called by button on index page.
+# it will pull all the Mars data from numerous web pages
+# and return it all as a single collection
+# that gets written to Mongo.
+# then home route gets re-called to render with newly
+# scraped data
 @app.route("/scrape")
 def scraper():
     mars = mongo.db.mars
     mars_data = scrape_mars.scrape()
     mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -17,7 +17,7 @@ def scrape():
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=False)
 
-    # URL of page to be scraped
+    # Mars News
     url = 'https://mars.nasa.gov/news/'
 
     browser.visit(url)
@@ -30,17 +30,19 @@ def scrape():
     # Create BeautifulSoup object
     soup = BeautifulSoup(html, 'html.parser')
 
+    # grab text of first article anchor in list
     news_title = soup.find("div", class_="list_text").a.text
 
     if debug_mode:
         print(news_title)
 
+    # grab text of first article teaser in list
     news_p = soup.find("div", class_="article_teaser_body").text
 
     if debug_mode:
         print(news_p)
 
-    # URL of page to be scraped
+    # Jet Propulsion Laboratory images
     base_url = 'https://www.jpl.nasa.gov'
     soup_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 
@@ -52,13 +54,15 @@ def scrape():
 
     soup = BeautifulSoup(html, 'html.parser')
 
+    # a "fancybox" button links to full size image
+    # get href link of that button
     img_anchor = soup.find("a", class_="button fancybox")
     featured_img_url = base_url + img_anchor.get("data-fancybox-href")
 
     if debug_mode:
         print(featured_img_url)
 
-    # URL of page to be scraped
+    # Mars weather Twitter
     base_url = 'https://twitter.com/marswxreport'
     soup_url = 'https://twitter.com/marswxreport?lang=en'
 
@@ -70,6 +74,10 @@ def scrape():
 
     soup = BeautifulSoup(html, 'html.parser')
 
+    # Not all tweets are weather
+    # but weather tweets seems to start with "InSight"
+    # so find_all tweet texts and iterate until we find most recent tweet
+    # that starts with "InSight"
     tweet_p = soup.find_all("p", class_="tweet-text")
 
     for tweet in tweet_p:
@@ -80,28 +88,33 @@ def scrape():
     if debug_mode:
         print(mars_weather)
 
+    # Mars facts
     facts_url = "https://space-facts.com/mars/"
 
+    # read html tables
     tables = pd.read_html(facts_url)
 
+    # second table is facts
     facts_table = tables[1]
+
+    # rename default unnamed columns
     facts_table = facts_table.rename(columns={0: "Description", 1: "Value"})
+
+    # drop default index and set it to first data column
     facts_table = facts_table.set_index("Description")
 
     if debug_mode:
         print(facts_table)
 
+    # make a new html table, but drop the newline characters
     facts_html = facts_table.to_html().replace('\n', '')
 
     if debug_mode:
         print(facts_html)
 
-    # The site for Mars hemisphere images referenced in homework returns error 404.
-    # I found a page at "The Planetary Society" that includes Mars hemisphere images
-    # I am using that.  Hopefully it does not go 404 soon!
-
-    # URL of page to be scraped
-    url = 'http://www.planetary.org/blogs/guest-blogs/bill-dunford/20140203-the-faces-of-mars.html'
+    # Hemisphere images
+    url_hemi_base = "https://astrogeology.usgs.gov"
+    url = url_hemi_base + "/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
     browser.visit(url)
 
@@ -112,20 +125,42 @@ def scrape():
 
     soup = BeautifulSoup(html, 'html.parser')
 
-    # All the hemisphere images at this site have a class of "img840"
-    images_list = soup.find_all("img", class_="img840")
-
+    # All hemisphere links are in divs classed "description"
+    links_list = soup.find_all("div", class_="description")
+    
     hemisphere_image_urls = []
 
-    for img in images_list:
-        img_title = img.get("alt")
-        img_url = img.get("src")
-        img_dict = {"title": img_title, "img_url": img_url}
-        hemisphere_image_urls.append(img_dict)
+    # loop through list of hemisphere links
+    for link in links_list:
+        # image title is text of anchor link
+        img_title = link.a.text
+    
+        # link to this hemisphere page
+        link_url = url_hemi_base + link.a.get("href")
+    
+        # visit this hemisphere
+        browser.visit(link_url)
 
+        # it takes a second for javascript to run and generate html of page
+        time.sleep(1)
+
+        link_html = browser.html
+    
+        soup_link = BeautifulSoup(link_html, 'html.parser')
+    
+        # get the src of wide-image class for full size hemisphere image
+        img_url = soup_link.find("img", class_="wide-image").get("src")
+        img_url = url_hemi_base + img_url
+    
+        # put title and url in a dictionary
+        img_dict = {"title": img_title, "img_url": img_url}
+    
+        # push dictionary into list of all hemispheres
+        hemisphere_image_urls.append(img_dict)
+    
     if debug_mode:
         print(hemisphere_image_urls)
-
+    # create a big dictionary with all mars data
     mars_dict = {
         "news_title": news_title,
         "news_p": news_p,
